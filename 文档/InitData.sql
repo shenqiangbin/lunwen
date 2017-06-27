d4811e998c507cbd503912549bfb7ebf008f0a9d
sqb <shenqiangbin@163.com>
2017-06-27 10:04:25 +0800

    编写Cache类

--- 文档/InitData.sql
+++
@@ -1,4 +0,0 @@
-insert user(UserCode,UserName,Password) values('admin','管理员','123456');
-insert user(UserCode,UserName,Password) values('user001','用户001','123456');
-insert user(UserCode,UserName,Password) values('user002','用户002','123456');
-insert user(UserCode,UserName,Password) values('user003','用户003','123456');
