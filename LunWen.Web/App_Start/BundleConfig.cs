e836b31ab863f12fb9cc53de26a4bd9dc88ef844
sqb <shenqiangbin@163.com>
2017-06-29 13:51:29 +0800

    添加Optimization

---
+++ LunWen.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace LunWen.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/upload.css").Include("~/Content/upload.css"));
+            bundles.Add(new ScriptBundle("").Include(""));
+
+            //@Scripts.Render("~/Scripts/admin/DateInput/WdatePicker.js")
+            //@Styles.Render("~/Content/autocomplete/jquery-ui.css")
+        }
+    }
+}
