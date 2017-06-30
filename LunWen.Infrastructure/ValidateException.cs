8893dad22a7bffffb0570b08fea57c0ac8289a9d
sqb <shenqiangbin@163.com>
2017-06-30 09:29:04 +0800

    添加简单的登陆

---
+++ LunWen.Infrastructure/ValidateException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunWen.Infrastructure
+{
+    public class ValidateException : Exception
+    {
+        public int Code { get; set; }
+
+        public ValidateException(int code, string msg) : base(msg)
+        {
+            this.Code = code;
+        }
+    }
+}
--- LunWen.Web/Controllers/HomeController.cs
+++ LunWen.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LunWen.Infrastructure.Cache;
+using LunWen.Infrastructure;
 
 namespace LunWen.Web.Controllers
 {
@@ -13,6 +14,55 @@ namespace LunWen.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string usercode, string password, string validateCode, string returnUrl)
+        {
+
+            try
+            {
+                ViewBag.UserCode = usercode;
+                ViewBag.Password = password;
+                ValidateIndex(usercode, password, validateCode);
+
+                if (usercode == "admin" && password == "123")
+                {
+                   return RedirectToAction("index", "user");
+                }
+                else
+                {
+                    ViewBag.Msg = "用户名或密码错误";
+                }
+            }
+            catch (ValidateException ex)
+            {
+                ViewBag.Msg = ex.Message;
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(ex);
+                ViewBag.Msg = "登录失败(500)";
+            }
+
+            return View();
+        }
+
+        private void ValidateIndex(string usercode, string password, string validateCode)
+        {
+            if (string.IsNullOrEmpty(usercode))
+                throw new ValidateException(400, "登录名不能为空");
+            if (string.IsNullOrEmpty(password))
+                throw new ValidateException(401, "密码不能为空");
+            if (string.IsNullOrEmpty(validateCode))
+                throw new ValidateException(402, "验证码不能为空");
+
+            if (Session["Code"] != null)
+            {
+                if (!string.Equals(validateCode, Session["Code"].ToString(), StringComparison.CurrentCultureIgnoreCase))
+                    throw new ValidateException(402, "验证码不正确");
+            }
         }
     }
 }
