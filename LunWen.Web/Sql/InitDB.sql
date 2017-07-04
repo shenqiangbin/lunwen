c03377b1a5254d119edda2d6c664f4b0b72a8dbd
sqb <shenqiangbin@163.com>
2017-07-04 15:57:08 +0800

    添加权限相关表

--- LunWen.Web/Sql/InitDB.sql
+++ LunWen.Web/Sql/InitDB.sql
@@ -13,6 +13,7 @@ CREATE TABLE `log` (
           `userid` varchar(45) DEFAULT NULL,
           PRIMARY KEY (`id`)
         ) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
+alter table log comment '错误日志';
 
 drop table if exists timelog;
 CREATE TABLE `timelog` (
@@ -30,6 +31,7 @@ CREATE TABLE `timelog` (
 		  `paraObj` varchar(2000) DEFAULT NULL,
           PRIMARY KEY (`id`)
         ) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
+alter table timelog comment '方法调用用时日志';
 
 drop table if exists User;
 create table User
@@ -49,3 +51,58 @@ create table User
 );
 
 alter table User comment '用户';
+
+
+drop table if exists Role;
+create table Role
+(
+   Id       	int not null auto_increment comment ' 唯一标识',
+   RoleCode		varchar(20) not null comment '角色code',
+   RoleName		nvarchar(50) not null comment '名称',
+   Level		int not null default -1 comment '级别',
+   Status		int not null default 1 comment '是否删除：1：未删除，0：已删除',
+   
+   primary key(Id) ,
+   unique (RoleCode),
+   unique (RoleName)
+);
+alter table Role comment '角色';
+
+drop table if exists UserRole;
+create table UserRole
+(
+   Id       	int not null auto_increment comment ' 唯一标识',
+   UserId		int not null comment '用户id',
+   RoleId		int not null comment '角色id',   
+   Status		int not null default 1 comment '是否删除：1：未删除，0：已删除',
+   
+   primary key(Id)    
+);
+alter table UserRole comment '用户&角色';
+
+
+drop table if exists RoleMenu;
+create table RoleMenu
+(
+   Id       	int not null auto_increment comment ' 唯一标识',
+   RoleId		int not null comment '角色id',
+   MenuId		int not null comment '菜单id',   
+   Status		int not null default 1 comment '是否删除：1：未删除，0：已删除',
+   
+   primary key(Id) 
+);
+alter table RoleMenu comment '角色&菜单';
+
+drop table if exists Menu;
+create table Menu
+(
+   Id       	int not null auto_increment comment ' 唯一标识',
+   ParentId		int not null comment '父节点',
+   Level		int not null comment '级别',
+   MenuUrl		varchar(50) not null comment 'URL地址',
+   MenuName		nvarchar(50) not null comment '菜单名称',   
+   Status		int not null default 1 comment '是否删除：1：未删除，0：已删除',
+   
+   primary key(Id)    
+);
+alter table Menu comment '菜单';
--- LunWen.Web/Sql/InitData.sql
+++ LunWen.Web/Sql/InitData.sql
@@ -7,3 +7,42 @@ insert user(UserCode,UserName,Salt,Password) values('user005','用户005','c667c
 insert user(UserCode,UserName,Salt,Password) values('user006','用户006','c667c7fb-fd99-4576-8f6d-8ac10e930514','96656905C76EFD9B4734106875CD8A59CCD495187A1BCDD88C9756632C48AA282527D58FCA9711F4BE6C7A1CB85ABDF70DAE0321B47B0B66E774CD0F92262837');
 insert user(UserCode,UserName,Salt,Password) values('user007','用户007','c667c7fb-fd99-4576-8f6d-8ac10e930514','96656905C76EFD9B4734106875CD8A59CCD495187A1BCDD88C9756632C48AA282527D58FCA9711F4BE6C7A1CB85ABDF70DAE0321B47B0B66E774CD0F92262837');
 insert user(UserCode,UserName,Salt,Password) values('user008','用户008','c667c7fb-fd99-4576-8f6d-8ac10e930514','96656905C76EFD9B4734106875CD8A59CCD495187A1BCDD88C9756632C48AA282527D58FCA9711F4BE6C7A1CB85ABDF70DAE0321B47B0B66E774CD0F92262837');
+
+INSERT INTO `thesisdb`.`role` (`RoleCode`, `RoleName`, `Level`, `Status`) VALUES ('SuperAdmin', '超级管理员', '1', '1');
+INSERT INTO `thesisdb`.`role` (`RoleCode`, `RoleName`, `Level`, `Status`) VALUES ('CoPerAdmin', '合作部管理员', '2', '1');
+INSERT INTO `thesisdb`.`role` (`RoleCode`, `RoleName`, `Level`, `Status`) VALUES ('SchoolAdmin', '学校管理员', '2', '1');
+
+
+INSERT INTO `thesisdb`.`menu` (`Id`,`ParentId`, `Level`, `MenuUrl`, `MenuName`, `Status`) VALUES ('1','0', '1', '/Manager/Index', '首页', '1');
+INSERT INTO `thesisdb`.`menu` (`Id`,`ParentId`, `Level`, `MenuUrl`, `MenuName`, `Status`) VALUES ('2','0', '1', '/ThesisMgr/Index', '论文管理', '1');
+INSERT INTO `thesisdb`.`menu` (`Id`,`ParentId`, `Level`, `MenuUrl`, `MenuName`, `Status`) VALUES ('3','0', '1', '/CostMgr/Index', '费用管理', '1');
+INSERT INTO `thesisdb`.`menu` (`Id`,`ParentId`, `Level`, `MenuUrl`, `MenuName`, `Status`) VALUES ('4','0', '1', '/NoticeMgr/Index', '通知公告', '1');
+INSERT INTO `thesisdb`.`menu` (`Id`,`ParentId`, `Level`, `MenuUrl`, `MenuName`, `Status`) VALUES ('5','0', '1', '/OrgMgr/Index', '架构管理', '1');
+
+
+INSERT INTO `thesisdb`.`menu` (`Id`,`ParentId`, `Level`, `MenuUrl`, `MenuName`, `Status`) VALUES ('6','2', '2', '/ThesisMgr/Check', '论文审核', '1');
+INSERT INTO `thesisdb`.`menu` (`Id`,`ParentId`, `Level`, `MenuUrl`, `MenuName`, `Status`) VALUES ('7','2', '2', '/ThesisMgr/BatchSubmit', '批量提交', '1');
+INSERT INTO `thesisdb`.`menu` (`Id`,`ParentId`, `Level`, `MenuUrl`, `MenuName`, `Status`) VALUES ('8','2', '2', '/ThesisMgr/Feedback', '加工反馈', '1');
+INSERT INTO `thesisdb`.`menu` (`Id`,`ParentId`, `Level`, `MenuUrl`, `MenuName`, `Status`) VALUES ('9','2', '2', '/ThesisMgr/Rewards', '薪酬管理', '1');
+
+
+/*管理员有所有权限*/
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('1', '1', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('1', '2', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('1', '3', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('1', '4', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('1', '5', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('1', '6', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('1', '7', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('1', '8', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('1', '9', '1');
+
+
+/*合作部管理员没有（首页、批量提交、架构管理）*/
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('2', '2', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('2', '3', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('2', '4', '1');
+
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('2', '6', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('2', '8', '1');
+INSERT INTO `thesisdb`.`rolemenu` (`RoleId`, `MenuId`, `Status`) VALUES ('2', '9', '1');
